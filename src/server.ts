a7951a3745e0e5155b05344f6376f271fdc64b4e
Herllon Cardoso <herllon.cardoso@govoll.com>
2021-04-19 08:49:31 -0300

    create get route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.json({message: 'Hello NLW 5'})
+})
+
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
 })
