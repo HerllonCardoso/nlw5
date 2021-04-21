643ae34878df670b5d413467af8e8a73567ebe04
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-04-21 12:36:28 -0300

    typeorm setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import express from "express";
+import express from 'express';
+
+import './database';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello NLW 5" });
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello NLW 5' });
 });
 
-app.post("/", (req, res) => {
-  return res.json({ message: "User signed up" });
+app.post('/', (req, res) => {
+  return res.json({ message: 'User signed up' });
 });
 
 app.listen(3333, () => {
-  console.log("Server is running on port 3333");
+  console.log('Server is running on port 3333');
 });
