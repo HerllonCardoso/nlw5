490ccac970649f7bc8759c0d2814094e326bbd81
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-04-21 13:12:13 -0300

    configuring Repository

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import express from 'express';
+import { routes } from './routes';
 
 import './database';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello NLW 5' });
-});
+app.use(express.json())
 
-app.post('/', (req, res) => {
-  return res.json({ message: 'User signed up' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
